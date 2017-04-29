1148384ab6649e13bfd694cc5b33f6cb740a58cf
Bernardo Gomes <bgomesdeabreu@gmail.com>
2017-04-29 11:51:58 -0300

    aba denuncia_ ok

---
+++ cnddh/export_to_excel/app.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+import xlsxwriter
+
+from cnddh.database import db
+from cnddh.models import Denuncia
+
+
+
+
+export_app = Blueprint('exportar-dados', __name__,url_prefix='/exportar-dados',
+                        template_folder='templates')
+
+
+LIMIT_ROW = 1048576
+
+
+def str_to_unicode_utf8(string):
+    return unicode(string,'utf-8')
+
+def normalize_str(string):
+    if string:
+        if isinstance(string, unicode):
+            return string
+        else:
+            return str_to_unicode_utf8(str(string))
+    else:
+        return ""
+
+def _create_cabecalho_denuncia(aba, negrito):
+    aba.write('A1', str_to_unicode_utf8('Denúncia ID'), negrito)
+    aba.write('B1', str_to_unicode_utf8('Número'), negrito)
+    aba.write('C1', str_to_unicode_utf8('Data Criação'), negrito)
+    
+    aba.write('D1', str_to_unicode_utf8('Data Denúncia'), negrito)
+    aba.write('E1', str_to_unicode_utf8('Status ID'), negrito)
+    
+    # aba.write('F1', '', negrito)
+    aba.write('G1', str_to_unicode_utf8('Fonte ID'), negrito)
+    aba.write('H1', str_to_unicode_utf8('Fonte'), negrito)
+    aba.write('I1', str_to_unicode_utf8('Protocolo'), negrito)
+    aba.write('J1', str_to_unicode_utf8('Resumo'), negrito)
+    aba.write('K1', str_to_unicode_utf8('Descrição'), negrito)
+    aba.write('L1', str_to_unicode_utf8('Observação'), negrito)
+    aba.write('M1', str_to_unicode_utf8('Tipo de Local'), negrito)
+    aba.write('N1', str_to_unicode_utf8('Endereço'), negrito)
+    aba.write('O1', str_to_unicode_utf8('Número'), negrito)
+    aba.write('P1', str_to_unicode_utf8('Complemento'), negrito)
+    
+    aba.write('Q1', str_to_unicode_utf8('Referência'), negrito)
+    aba.write('R1', str_to_unicode_utf8('Bairro'), negrito)
+    aba.write('S1', str_to_unicode_utf8('Cidade'), negrito)
+    aba.write('T1', str_to_unicode_utf8('CEP'), negrito)
+    aba.write('U1', str_to_unicode_utf8('Estado'), negrito)
+    aba.write('V1', str_to_unicode_utf8('País'), negrito)
+    
+    #Largura
+    aba.set_column('A:G', 13)
+    aba.set_column('H:H', 25)
+    aba.set_column('I:I', 13)
+    aba.set_column('J:L', 25)
+    aba.set_column('M:V', 20)
+
+    #Filtro
+    aba.autofilter('A1:V1')
+
+
+def _criar_aba_denuncia(planilha, filtros, negrito, date_format):
+    aba = planilha.add_worksheet(u'Denúncias')
+    limit_quantidade = 0
+
+    _create_cabecalho_denuncia(aba, negrito)
+
+    for index, denuncia in enumerate(db.session.query(Denuncia).all(),start=1):
+        if index > (LIMIT_ROW + limit_quantidade * LIMIT_ROW):
+            aba = planilha.add_worksheet(str_to_unicode_utf8('Denúncias_'+str(1+limit_quantidade)))
+            _create_cabecalho_denuncia(aba, negrito)
+            limit_quantidade +=1
+        aba.write_number(index - limit_quantidade * LIMIT_ROW, 0, denuncia.id)
+        aba.write(index - limit_quantidade * LIMIT_ROW, 1, denuncia.numero)
+        aba.write_datetime(index - limit_quantidade * LIMIT_ROW, 2, denuncia.dtcriacao, date_format)
+        aba.write_datetime(index - limit_quantidade * LIMIT_ROW, 3, denuncia.dtdenuncia, date_format)
+        aba.write(index - limit_quantidade * LIMIT_ROW, 4, denuncia.status_id)
+        # aba.write(index, 5, denuncia.status)
+        
+        aba.write(index - limit_quantidade * LIMIT_ROW, 6, denuncia.tipofonte_id)
+        aba.write_string(index - limit_quantidade * LIMIT_ROW, 7, normalize_str(denuncia.fonte))
+        aba.write_string(index - limit_quantidade * LIMIT_ROW, 8, normalize_str(denuncia.protocolo))
+        aba.write_string(index - limit_quantidade * LIMIT_ROW, 9, normalize_str(denuncia.resumo))
+        aba.write_string(index - limit_quantidade * LIMIT_ROW, 10, normalize_str(denuncia.descricao))
+        aba.write_string(index - limit_quantidade * LIMIT_ROW, 11, normalize_str(denuncia.observacao))
+        aba.write(index - limit_quantidade * LIMIT_ROW, 12, denuncia.tipolocal)
+        aba.write(index - limit_quantidade * LIMIT_ROW, 13, denuncia.endereco)
+        aba.write(index - limit_quantidade * LIMIT_ROW, 14, denuncia.num)
+        aba.write(index - limit_quantidade * LIMIT_ROW, 15, denuncia.complemento)
+        aba.write(index - limit_quantidade * LIMIT_ROW, 16, denuncia.referencia)
+        aba.write(index - limit_quantidade * LIMIT_ROW, 17, denuncia.bairro)
+        aba.write(index - limit_quantidade * LIMIT_ROW, 18, denuncia.cidade)
+        aba.write(index - limit_quantidade * LIMIT_ROW, 19, denuncia.cep)
+        aba.write(index - limit_quantidade * LIMIT_ROW, 20, denuncia.estado)
+        aba.write(index - limit_quantidade * LIMIT_ROW, 21, denuncia.pais)
+
+       
+
+@export_app.route('/criar-planilha')
+def criar_planilha():
+    try:
+        date_now = datetime.now().strftime("_%Y_%m_%d_%H_%M_%S")
+        workbook = xlsxwriter.Workbook('planilha_de_dados'+date_now+'.xlsx')
+        
+        #Format
+        negrito = workbook.add_format({'bold': 1})
+        date_format = workbook.add_format({'num_format': 'dd/mm/yyyy'})
+        #Format
+
+        _criar_aba_denuncia(workbook, [], negrito, date_format)
+        workbook.close()
+
+        return "Gerada."
+    except TemplateNotFound:
+        abort(404)
